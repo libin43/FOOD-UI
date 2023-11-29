421df59a63cc7c990106912b053a912d6098788e
Libin Biji <libinbiji43@gmail.com>
2023-11-29 23:22:58 +0530

    component: Home add

---
+++ src/components/Home/HomeBanner.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import emojiHome from '../../assets/emojiHome.png'
+import './HomeBanner.css'
+
+export const HomeBanner = () => {
+  return (
+    <div id="banner-container">
+        <div id="sm-heading">
+            <span id="sm-heading-text">Unique Curry Powders</span>
+        </div>
+        <div id="title-container">
+            <div className='check'>Wake Up Your <img className="emo-smile" src={emojiHome} alt="smiley" /></div>
+            <div>Taste Buds With Us</div>
+        </div>
+    </div>
+  )
+}
---
+++ src/components/Home/HomeBrand.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import imgSpinLg from '../../assets/imgSpinLg.png'
+// import imgSpinSm from '../../assets/imgSpinSm.png'
+import './HomeBrand.css'
+
+export const HomeBrand = () => {
+  return (
+    <div id="brand-container">
+        <img id="img-brand" src={imgSpinLg} alt="" />
+    </div>
+  )
+}
