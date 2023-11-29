7132f724b365722043ff7f1bd9c657b63e5abc99
Libin Biji <libinbiji43@gmail.com>
2023-11-29 23:24:43 +0530

    page: Page dir add

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { NavSidebarContainer } from '../components/Navbar/NavSidebarContainer'
+
+export const HomePage = () => {
+  return (
+    <div>
+        <NavSidebarContainer/>
+    </div>
+  )
+}
