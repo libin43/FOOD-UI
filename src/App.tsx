bcbdb8d3ebd0f4e18f2bc5bb3820f0fb2806a73b
Libin Biji <libinbiji43@gmail.com>
2023-11-30 21:44:03 +0530

    update: component add

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 
 // import viteLogo from '/vite.svg'
 import './App.css'
+import { HomeBanner } from './components/Home/HomeBanner'
+import { HomeBrand } from './components/Home/HomeBrand'
 import { NavSidebarContainer } from './components/Navbar/NavSidebarContainer'
+import { ProvinceSelect } from './components/Province/ProvinceSelect'
 
 function App() {
 
   return (
     <>
     <NavSidebarContainer/>
+    <HomeBanner/>
+    <div className="homesection">
+      {/* haijhi */}
+    <HomeBrand/>
+    <ProvinceSelect/>
+    </div>
     </>
   )
 }
